5f289b275ef7575f9ce5ff0efbfe204e2e80a01e
Jessica Ferrari <>
2021-04-04 02:30:11 +0000

    Added image gallery & random button

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import requests
 
 #imports a dictionary of data from dog_breeds.py and "prettifies", or styles, the dog names when they appear in the HTML page
 from dog_breeds import prettify_dog_breed
@@ -9,10 +11,29 @@ app = Flask("app")
 def check_breed(breed):
   return "/".join(breed.split("-"))
 
-@app.route("/")
-def hello_world():
-  return render_template("dogs.html")
+@app.route("/", methods=["GET","POST"])
+def dog_images_gallery():
+  errors = []
+  if request.method == "POST":
+    breed = request.form.get("breed")
+    number = request.form.get("number")
+    if not breed:
+      errors.append("Oops! Please choose a breed.")
+    if not number:
+      errors.append("Oops! Please choose a number.")
+    if breed and number:
+      response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/" + number)
+      data = response.json()
+      dog_images = data["message"]
+      return render_template("dogs.html", images=dog_images, breed=prettify_dog_breed(breed), errors=[])
+  return render_template("dogs.html", images=[], breed="", errors=errors)
 
+@app.route("/random", methods=["POST"])
+def get_random():
+  response = requests.get("https://dog.ceo/api/breeds/image/random")
+  data = response.json()
+  dog_images = [data["message"]]
+  return render_template("dogs.html", images=dog_images)
 
 app.debug = True
 app.run(host='0.0.0.0', port=8080)
